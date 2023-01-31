70ca1533eaad1309bd40a446b4f4523b691945b0
Luis Esquivel <luisesquivel97@hotmail.com>
2023-01-31 15:12:02 -0600

    Prueba IA

---
+++ submit_form.php
@@ -0,0 +1,4 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $equipment_name = $_POST["equipment_name"];
+    $equipment_type = $_POST["equipment_type"];
+    $manufacturer = $_POST["manufacturer"];
